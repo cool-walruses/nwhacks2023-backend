7e6833ead6093b3b7d63e5d7c8ef693d66c08fbd
DFriend01 <devon.fri.2000@gmail.com>
2023-01-21 19:57:12 -0800

    Remove commented code

--- app.py
+++ app.py
@@ -7,26 +7,6 @@ def create_app(test_config=None):
     app = Flask(__name__)
     openai.api_key = os.getenv("OPENAI_API_KEY")
 
-    """
-    app.config.from_mapping(
-        SECRET_KEY='dev',
-        # DATABASE=os.path.join(app.instance_path, 'flaskr.sqlite'),
-    )
-
-    if test_config is None:
-        # load the instance config, if it exists, when not testing
-        app.config.from_pyfile('config.py', silent=True)
-    else:
-        # load the test config if passed in
-        app.config.from_mapping(test_config)
-
-    # ensure the instance folder exists
-    try:
-        os.makedirs(app.instance_path)
-    except OSError:
-        pass
-    """
-
     from flaskapp import generator
 
     # a simple page that says hello
@@ -37,8 +17,6 @@ def create_app(test_config=None):
     app.register_blueprint(generator.bp)
     return app
 
-
 app = create_app()
-
 if __name__ == '__main__':
     app.run()
